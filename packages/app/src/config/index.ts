dd1cad46cf998b3eac03415d579a475b1e3b33b7
cirolee <ciromail@126.com>
2024-08-06 12:40:18 +0800

    feat: add some examples

--- packages/app/src/config/index.ts
+++ packages/app/src/config/index.ts
@@ -1,5 +1,7 @@
 import noticeBarImg from '@/assets/images/noticeBar@2x.webp';
 import drawerImg from '@/assets/images/drawer@2x.webp';
+import tabBarImg from '@/assets/images/tabbar@2x.webp';
+import pulseImg from '@/assets/images/pulse@2x.webp';
 import type { ExampleCard } from '@/types';
 
 export const exampleList: ExampleCard[] = [
@@ -7,7 +9,7 @@ export const exampleList: ExampleCard[] = [
     id: 'notice-bar',
     name: 'NoticeBar',
     cover: noticeBarImg,
-    url: 'https://codesandbox.io/p/sandbox/noticebar-y2ylrc',
+    url: 'https://codesandbox.io/p/sandbox/notice-bar-v4zrdc',
     desc: 'infinite scrolling notice bar',
     tags: ['useAnimate']
   },
@@ -16,7 +18,23 @@ export const exampleList: ExampleCard[] = [
     name: 'Drawer',
     cover: drawerImg,
     desc: 'the application of view transition, this is used as Drawer component',
-    url: 'https://codesandbox.io/p/sandbox/serene-borg-rggcfz',
+    url: 'https://codesandbox.io/p/sandbox/drawer-g39q59',
     tags: ['useMotion']
+  },
+  {
+    id: 'tabBar',
+    name: 'TabBar',
+    cover: tabBarImg,
+    desc: 'svg path animation, used in tabBar component',
+    url: 'https://codesandbox.io/p/sandbox/tabbar-9pgyq5',
+    tags: ['useLineDraw', 'svg']
+  },
+  {
+    id: 'pulse',
+    name: 'Pulse',
+    cover: pulseImg,
+    desc: 'pulse like animation',
+    url: 'https://codesandbox.io/p/sandbox/pulse-9w4cj8',
+    tags: ['useAnimate', 'svg']
   }
 ];
